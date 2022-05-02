8308e4f418aad64c8e3312fe2e061befc52ec007
yanggfann <598708868@qq.com>
2022-05-02 08:41:08 +0800

    init springboot and gradle

---
+++ src/main/java/com/example/precharge/PrechargeApplication.java
@@ -0,0 +1,13 @@
+package com.example.precharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrechargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrechargeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/precharge/PrechargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.precharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
